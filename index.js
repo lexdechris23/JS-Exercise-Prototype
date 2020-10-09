dbb3ddad4f76e1e0f88cd7a554738a6d7c6797d1
lexdechris23 <70167097+lexdechris23@users.noreply.github.com>
2020-10-08 19:35:35 -0500

    modified injex.js

--- index.js
+++ index.js
@@ -39,10 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood);
+  }
+};
+
+Person.prototype.poop = function () {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function () {
+  return `${this.age} ${this.name}`
+};
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -57,9 +72,21 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function (gallons) {
+   return this.tank = gallons + this.tank;
+    }
+
+Car.prototype.drive = function (distance) {
+  
+}
+
 
 /*
   TASK 3
@@ -68,18 +95,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
-}
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+  }
+  
+  Baby.prototype = Object.create(Person.prototype);
+  
+  
+  Baby.prototype.play = function () {
+    return `Playing with ${this.favoriteToy}`;
+  }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Object Binding- When 'this' is used in the global scope.
+  2. Implicit Binding-  Occurs when dot notation is sed to invoke a function.
+  3. New Binding: Refers to instance of object that is created and returned by constructor function.
+  4. Explicit Binding: explicit binding of this occurs when .call(), .apply(), or .bind() are used on a function.
 */
 
 
